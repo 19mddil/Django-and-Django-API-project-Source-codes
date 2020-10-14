0dc78a65d5c663b8e2ff0de1e962385e439f06e4
19mddil <mddilshadul98@gmail.com>
2020-10-14 12:03:41 +0600

    CRUD-create reade update delete works using class based view

--- vincent_projects/blog/blog/views.py
+++ vincent_projects/blog/blog/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from django.views.generic.edit import CreateView #new
+from django.views.generic.edit import CreateView,UpdateView,DeleteView #new
 from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
 
-class BlogListView(ListView):
+class BlogListView(ListView): #read
+
 	model = Post
 	template_name = 'blog/home.html'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(DetailView): #read
+
 	model = Post
 	template_name = 'blog/post_detail.html'
 
-class BlogCreateView(CreateView):
+class BlogCreateView(CreateView): #create
+
 	model = Post
 	template_name = 'blog/post_new.html'
 	fields = ['title','author','body']
+
+class BlogUpdateView(UpdateView): #update
+
+	model = Post
+	template_name = 'blog/post_edit.html'
+	fields = ['title','body']
+
+class BlogDeleteView(DeleteView): #delete
+
+	model = Post
+	template_name = 'blog/post_delete.html'
+	success_url = reverse_lazy('home')
+
