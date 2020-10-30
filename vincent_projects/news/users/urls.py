e8b177533998f108ad669a585af572e8e767d630
19mddil <mddilshadul98@gmail.com>
2020-10-30 23:10:34 +0600

    added custom templates for password change and login page

--- vincent_projects/news/users/urls.py
+++ vincent_projects/news/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView,PasswordChangeView,LoginView,PasswordChangeDoneView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('signup/',SignUpView.as_view(),name='signup'),
+	path('password_change/', PasswordChangeView.as_view() , name='password_change'),
+
+	path('password_change/done/', PasswordChangeDoneView.as_view() , name='password_change_done'),
+	path('login/',LoginView.as_view(),name='login'),
 ]
--- vincent_projects/news/users/views.py
+++ vincent_projects/news/users/views.py
@@ -1,8 +1,19 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth import views as auth_views
 
 class SignUpView(CreateView):
 	form_class = CustomUserCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'users/signup.html'
+
+
+class PasswordChangeView(auth_views.PasswordChangeView):
+    template_name = 'users/registration/password_change_form.html'
+
+class LoginView(auth_views.LoginView):
+	template_name = 'users/registration/login.html'
+
+class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+	template_name = 'users/registration/password_change_done.html'
