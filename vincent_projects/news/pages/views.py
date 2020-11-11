11eb916751d5f3f4520eec43c94d21093d3f2d6c
19mddil <mddilshadul98@gmail.com>
2020-11-11 13:40:04 +0600

    now anyone can not delete or edit other contents

--- vincent_projects/news/pages/views.py
+++ vincent_projects/news/pages/views.py
@@ -1,4 +1,15 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin,TemplateView):
 	template_name = 'users/home.html'
+	login_url = 'login' #new
+	permission_denied_message = 'You have to be logged in to access that page'
+
+	def dispatch(self, request):
+	    if not request.user.is_authenticated:
+	        messages.add_message(request, messages.WARNING,
+	                             self.permission_denied_message)
+	        return self.handle_no_permission()
+	    return super(LoginRequiredMixin,self).dispatch(request)
